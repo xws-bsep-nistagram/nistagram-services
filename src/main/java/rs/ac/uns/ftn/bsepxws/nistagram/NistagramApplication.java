40ef0302feec7c5eae0c30e73c28fc732397e7ed
sStrbacki <sasa.strbacki@protonmail.com>
2021-04-13 13:16:14 +0200

    Spring project initialized

---
+++ src/main/java/rs/ac/uns/ftn/bsepxws/nistagram/NistagramApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bsepxws.nistagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NistagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NistagramApplication.class, args);
+	}
+
+}
