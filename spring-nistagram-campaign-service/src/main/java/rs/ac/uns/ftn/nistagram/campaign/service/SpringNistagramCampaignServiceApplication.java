a0267dc2b5516146af85d97bfc21036033809d16
sStrbacki <sasa.strbacki@protonmail.com>
2021-05-12 23:10:22 +0200

    Package names standardized

---
+++ spring-nistagram-campaign-service/src/main/java/rs/ac/uns/ftn/nistagram/campaign/service/SpringNistagramCampaignServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.nistagram.campaign.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNistagramCampaignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNistagramCampaignServiceApplication.class, args);
+    }
+
+}
---
+++ spring-nistagram-media-service/src/main/java/rs/ac/uns/ftn/nistagram/media/service/SpringNistagramMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.nistagram.media.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNistagramMediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNistagramMediaServiceApplication.class, args);
+    }
+
+}
