c0e8a0f2bf10a236a360d060df6389b163f72db8
sStrbacki <sasa.strbacki@protonmail.com>
2021-05-14 21:21:02 +0200

    Notification service added

---
+++ spring-nistagram-notification-service/src/main/java/rs/ac/uns/ftn/nistagram/notification/SpringNistagramNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.nistagram.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNistagramNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNistagramNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ spring-nistagram-notification-service/src/test/java/rs/ac/uns/ftn/nistagram/notification/SpringNistagramNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.nistagram.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNistagramNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
