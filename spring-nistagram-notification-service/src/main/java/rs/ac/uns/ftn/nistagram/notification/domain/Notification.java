79877ed2657067428913a45516d050ae7e1586de
sStrbacki <sasa.strbacki@protonmail.com>
2021-05-14 22:25:43 +0200

    Notification service initial model added

---
+++ spring-nistagram-notification-service/src/main/java/rs/ac/uns/ftn/nistagram/notification/domain/Notification.java
@@ -0,0 +1,15 @@
+package rs.ac.uns.ftn.nistagram.notification.domain;
+
+import java.time.Instant;
+
+public class Notification {
+
+    private Instant timestamp;
+    private String username;
+    private String causerUsername;
+    private String content;
+    private NotificationType notificationType;
+    private String postId;
+    private String messageContent;
+
+}
---
+++ spring-nistagram-notification-service/src/main/java/rs/ac/uns/ftn/nistagram/notification/domain/NotificationType.java
@@ -0,0 +1,7 @@
+package rs.ac.uns.ftn.nistagram.notification.domain;
+
+public enum NotificationType {
+    CHAT,
+    CONTENT,
+    FOLLOWING
+}
