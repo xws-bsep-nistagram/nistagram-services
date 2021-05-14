eb069f139e0434493e3760de2e02e4e1ba2cab90
sStrbacki <sasa.strbacki@protonmail.com>
2021-05-14 21:30:28 +0200

    Feed service initial domain model added

---
+++ spring-nistagram-feed-service/src/main/java/rs/ac/uns/ftn/nistagram/feed/domain/UserFeedEntry.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.nistagram.feed.domain;
+
+import java.time.Instant;
+
+public class UserFeedEntry {
+
+    private String username;
+    private Instant createdAt;
+    private String postId;
+
+
+}
