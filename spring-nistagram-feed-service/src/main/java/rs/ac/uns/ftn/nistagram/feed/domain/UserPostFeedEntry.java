8bbcc03faaa71b7b57c1899a1a6a023965645d2f
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-05-23 19:39:16 +0200

    Feed service extending UserFeedEntry

--- spring-nistagram-feed-service/src/main/java/rs/ac/uns/ftn/nistagram/feed/domain/UserPostFeedEntry.java
+++ spring-nistagram-feed-service/src/main/java/rs/ac/uns/ftn/nistagram/feed/domain/UserPostFeedEntry.java
@@ -1,6 +1,6 @@
 package rs.ac.uns.ftn.nistagram.feed.domain;
 
-public class UserPostFeedEntry extends UserFeedEntry{
+public class UserPostFeedEntry extends UserFeedEntry {
 
     private String postId;
 
--- spring-nistagram-feed-service/src/main/java/rs/ac/uns/ftn/nistagram/feed/domain/UserStoryFeedEntry.java
+++ spring-nistagram-feed-service/src/main/java/rs/ac/uns/ftn/nistagram/feed/domain/UserStoryFeedEntry.java
@@ -1,6 +1,6 @@
 package rs.ac.uns.ftn.nistagram.feed.domain;
 
-public class UserStoryFeedEntry {
+public class UserStoryFeedEntry extends UserFeedEntry {
 
     private String storyId;
 
