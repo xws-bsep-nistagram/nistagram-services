a89b42e13da9bab963286b1fc77465730f670b64
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-05-25 20:55:30 +0200

    Auth domain entities

--- spring-nistagram-auth-service/src/main/java/rs/ac/uns/ftn/nistagram/auth/domain/Credentials.java
+++ spring-nistagram-auth-service/src/main/java/rs/ac/uns/ftn/nistagram/auth/domain/Credentials.java
@@ -1,11 +1,68 @@
 package rs.ac.uns.ftn.nistagram.auth.domain;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class Credentials {
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Credentials implements UserDetails {
+    @Id
     private String username;
     private String passwordHash;
+    @Column(unique = true)
     private String email;
     private Boolean activated;
+    @ManyToMany
+    @JoinTable
     private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> role.getPermissions())
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return activated;
+    }
 }
