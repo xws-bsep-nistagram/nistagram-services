88e82990de3fd07f242ef4fa220114440b163b6e
sStrbacki <sasa.strbacki@protonmail.com>
2021-06-12 22:11:53 +0200

    Cors configured on all microservices

--- spring-nistagram-user-graph-service/src/main/java/rs/ac/uns/ftn/nistagram/user/graph/controllers/payload/UserRelationshipRequest.java
+++ spring-nistagram-user-graph-service/src/main/java/rs/ac/uns/ftn/nistagram/user/graph/controllers/payload/UserRelationshipRequest.java
@@ -2,11 +2,13 @@ package rs.ac.uns.ftn.nistagram.user.graph.controllers.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
 public class UserRelationshipRequest {
 
     @NotBlank(message = "Username is mandatory")
